cc13f1f14664826ca89cd12728614e09ea03a77d
Juan Goovaerts <juangoovaerts@gmail.com>
2020-07-19 21:39:20 -0500

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,55 +14,95 @@
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="bg-gray-100 h-screen antialiased leading-none">
-<div class="flex flex-col">
-    @if(Route::has('login'))
-        <div class="absolute top-0 right-0 mt-4 mr-4">
-            @auth
-                <a href="{{ url('/home') }}" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase">{{ __('Home') }}</a>
-            @else
-                <a href="{{ route('login') }}" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase pr-6">{{ __('Login') }}</a>
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase">{{ __('Register') }}</a>
-                @endif
-            @endauth
+<div class="bg-gray-50">
+    <div class="relative overflow-hidden">
+        <div class="block absolute inset-y-0 h-full w-full">
+            <div class="relative h-full">
+                <svg class="absolute right-full transform translate-y-1/3 translate-x-1/4 md:translate-y-1/2 sm:translate-x-1/2 lg:translate-x-full" width="404" height="784" fill="none" viewBox="0 0 404 784">
+                    <defs>
+                        <pattern id="e229dbec-10e9-49ee-8ec3-0286ca089edf" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                        </pattern>
+                    </defs>
+                    <rect width="404" height="784" fill="url(#ad9a0a02-b58e-4a1d-8c36-1b649889af63)" />
+                </svg>
+                <svg class="absolute left-full transform -translate-y-3/4 -translate-x-1/4 sm:-translate-x-1/2 md:-translate-y-1/2 lg:-translate-x-3/4" width="404" height="784" fill="none" viewBox="0 0 404 784">
+                    <defs>
+                        <pattern id="d2a68204-c383-44b1-b99f-42ccff4e5365" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                        </pattern>
+                    </defs>
+                    <rect width="404" height="784" fill="url(#478e97d6-90df-4a89-8d63-30fdbb3c7e57)" />
+                </svg>
+            </div>
         </div>
-    @endif
 
-    <div class="min-h-screen flex items-center justify-center">
-        <div class="flex flex-col justify-around h-full">
-            <div>
-                <h1 class="text-gray-600 text-center font-light tracking-wider text-5xl mb-6">
-                    {{ config('app.name', 'Laravel') }}
-                </h1>
-                <ul class="list-reset">
-                    <li class="inline pr-8">
-                        <a href="https://laravel.com/docs" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="Documentation">Documentation</a>
-                    </li>
-                    <li class="inline pr-8">
-                        <a href="https://laracasts.com" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="Laracasts">Laracasts</a>
-                    </li>
-                    <li class="inline pr-8">
-                        <a href="https://laravel-news.com" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="News">News</a>
-                    </li>
-                    <li class="inline pr-8">
-                        <a href="https://nova.laravel.com" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="Nova">Nova</a>
-                    </li>
-                    <li class="inline pr-8">
-                        <a href="https://forge.laravel.com" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="Forge">Forge</a>
-                    </li>
-                    <li class="inline pr-8">
-                        <a href="https://vapor.laravel.com" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="Vapor">Vapor</a>
-                    </li>
-                    <li class="inline pr-8">
-                        <a href="https://github.com/laravel/laravel" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="GitHub">GitHub</a>
-                    </li>
-                    <li class="inline pr-8">
-                        <a href="https://tailwindcss.com" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="Tailwind Css">Tailwind CSS</a>
-                    </li>
-                </ul>
+        <div class="relative pt-6 pb-12 lg:pb-20">
+            <div class="max-w-screen-xl mx-auto px-4 sm:px-6">
+                <nav class="relative flex items-center justify-between sm:h-10">
+                    <div class="flex items-center flex-1 md:inset-y-0 md:left-0">
+                        <div class="flex items-center justify-between w-full md:w-auto">
+                            <a href="#" aria-label="Home">
+                                <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-on-white.svg" alt="Logo">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
+                        <span class="inline-flex rounded-md shadow">
+                          <a href="#" class="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo active:bg-gray-50 active:text-indigo-700 transition duration-150 ease-in-out">
+                            Log in
+                          </a>
+                        </span>
+                    </div>
+                </nav>
+            </div>
+
+            <div class="mt-10 mx-auto max-w-screen-xl px-4 sm:px-6 md:mt-16 lg:mt-20">
+                <div class="text-center">
+                    <h2 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+                        Prueba técnica
+                        <br>
+                        <span class="text-indigo-600">Stradata</span>
+                    </h2>
+                    <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+                        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="relative">
+            <div class="absolute inset-0 flex flex-col">
+                <div class="flex-1"></div>
+                <div class="flex-1 w-full bg-gray-800"></div>
+            </div>
+            <div class="max-w-screen-xl mx-auto px-4 sm:px-6">
+                <img class="relative rounded-lg shadow-lg" src="/img/hero.png" alt="App screenshot">
+            </div>
+        </div>
+    </div>
+    <div class="bg-gray-800">
+        <div class="max-w-screen-xl mx-auto pt-16 pb-20 px-4 sm:px-6 md:pb-24 lg:px-8">
+            <h3 class="text-center text-gray-400 text-sm font-semibold uppercase tracking-wide">Trusted by over 26,000 forward-thinking companies</h3>
+            <div class="mt-8 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
+                <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+                    <img class="h-12" src="https://tailwindui.com/img/logos/tuple-logo.svg" alt="Tuple">
+                </div>
+                <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+                    <img class="h-12" src="https://tailwindui.com/img/logos/mirage-logo.svg" alt="Mirage">
+                </div>
+                <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+                    <img class="h-12" src="https://tailwindui.com/img/logos/statickit-logo.svg" alt="StaticKit">
+                </div>
+                <div class="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
+                    <img class="h-12" src="https://tailwindui.com/img/logos/transistor-logo.svg" alt="Transistor">
+                </div>
+                <div class="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
+                    <img class="h-12" src="https://tailwindui.com/img/logos/workcation-logo.svg" alt="Workcation">
+                </div>
             </div>
         </div>
     </div>
 </div>
+
 </body>
 </html>
