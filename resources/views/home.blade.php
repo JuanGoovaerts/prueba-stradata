d44a018a0dca9f07dec528ef68020c18903b12da
Juan Goovaerts <juangoovaerts@gmail.com>
2020-07-19 21:49:10 -0500

    wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -70,10 +70,10 @@
                         <thead>
                         <tr>
                             <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                Name
+                                Nombre
                             </th>
                             <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                                Role
+                                Rol
                             </th>
                             <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                 Lugar
